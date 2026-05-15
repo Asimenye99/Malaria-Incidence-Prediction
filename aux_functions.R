d682a7b860324430714321de1b4fdd9b97531efd
Victor Felix <victorfelix@s172-20-49-h186.paws.uga.edu>
2026-05-15 15:44:51 -0400

    in progress

--- aux_functions.R
+++ aux_functions.R
@@ -64,9 +64,12 @@ rolling_baseline <- function(data,
   return(rolling_incidence)
 }
 
-# Fit ARIMA models
-fit_SARIMA <- function(df) {
+
+
+fit_SARIMA <- function(df, SARIMAX = FALSE, xreg_col = NULL) {
+  
   all_dates <- seq(min(df$Date), max(df$Date), by = "month")
+  
   df2 <- right_join(df, tibble(Date = all_dates), by = "Date") %>%
     arrange(Date) %>%
     mutate(incidence_per_1k = replace_na(incidence_per_1k, 0))
@@ -79,15 +82,38 @@ fit_SARIMA <- function(df) {
   
   xreg_mat <- NULL
   
-  fit <- tryCatch(
-    auto.arima(
-      ts_d,
-      seasonal = TRUE,
-      stepwise = FALSE,
-      approximation = FALSE
-    ),
-    error = function(e) NULL
-  )
+  # Build xreg only if SARIMAX = TRUE
+  if (SARIMAX) {
+    
+    if (is.null(xreg_col)) {
+      stop("You must provide xreg_col when SARIMAX = TRUE")
+    }
+    
+    xreg_mat <- as.matrix(df2[, xreg_col, drop = FALSE])
+    
+    fit <- tryCatch(
+      auto.arima(
+        ts_d,
+        xreg = xreg_mat,
+        seasonal = TRUE,
+        stepwise = FALSE,
+        approximation = FALSE
+      ),
+      error = function(e) NULL
+    )
+    
+  } else {
+    
+    fit <- tryCatch(
+      auto.arima(
+        ts_d,
+        seasonal = TRUE,
+        stepwise = FALSE,
+        approximation = FALSE
+      ),
+      error = function(e) NULL
+    )
+  }
   
   if (is.null(fit)) return(NULL)
   
@@ -119,6 +145,8 @@ fit_SARIMA <- function(df) {
     xreg = xreg_mat
   )
 }
+
+
 
 # Forecasts SARIMA models
 forecast_SARIMAs <- function(model_list, h = 4, future_xreg_list = NULL) {
@@ -159,16 +187,87 @@ forecast_SARIMAs <- function(model_list, h = 4, future_xreg_list = NULL) {
 }
 
 # Rolling SARIMA
-rolling_sarima <- function(national_incidence, n_windows = 23) {
+rolling_sarima <- function(
+    national_incidence,
+    n_windows = 23,
+    SARIMAX = FALSE,
+    xreg_col = NULL
+) {
   
   rolling_incidence <- list()
   
   for (i in 1:n_windows) {
+    
     incidence_window <- national_incidence[(1 + i):(60 + i), ]
-    rolling_incidence[[i]] <- fit_SARIMA(incidence_window)
+    
+    rolling_incidence[[i]] <- fit_SARIMA(
+      df = incidence_window,
+      SARIMAX = SARIMAX,
+      xreg_col = xreg_col
+    )
+    
     print(i)
   }
   
   return(rolling_incidence)
 }
 
+forecast_baselines <- function(model_list, h = 4) {
+  
+  fc_list <- vector("list", length(model_list))
+  
+  for (i in seq_along(model_list)) {
+    
+    obj <- model_list[[i]]
+    
+    if (is.null(obj) || is.null(obj$model)) {
+      fc_list[[i]] <- NULL
+      next
+    }
+    
+    fc_list[[i]] <- tryCatch({
+      
+      forecast::forecast(obj$model, h = h)
+      
+    }, error = function(e) {
+      message("Forecast failed for baseline model ", i, ": ", e$message)
+      NULL
+    })
+  }
+  
+  names(fc_list) <- paste0("window_", seq_along(model_list))
+  
+  return(fc_list)
+}
+
+# get forecasted quantiles
+
+get_forecasted_quantiles <- function(forecast_list) {
+  
+  probabilities <- c(
+    0.01, 0.025,
+    seq(0.05, 0.95, by = 0.05),
+    0.975, 0.99
+  )
+  
+  lapply(forecast_list, function(fc) {
+    
+    if (is.null(fc)) return(NULL)
+    
+    # Recover SE from the 95% interval
+    se <- (fc$upper[, "95%"] - fc$mean) / qnorm(0.975)
+    
+    qmat <- sapply(probabilities, function(p) {
+      qnorm(
+        p,
+        mean = fc$mean,
+        sd = se
+      )
+    })
+    
+    colnames(qmat) <- probabilities
+    rownames(qmat) <- paste0("h", seq_along(fc$mean))
+    
+    qmat
+  })
+}
